1d3a34f98ec66b16984cc21ee7ba9df7f9da2c45
inwaytosenior <papatu10u18@gmail.com>
2024-05-15 16:29:06 -0600

    Se definio la tabla con 3 tablas, profesores y alu mnos que sacan la contrasena de la tabla passw

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,31 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+const passw = defineTable({
+  columns: {
+    username: column.text({ primaryKey: true }),
+    password: column.text()
+  } 
+});
+
+const Profesor = defineTable({
+
+  columns:{
+    id_profesor: column.number({ primaryKey: true }),
+    username: column.text({ references: () => passw.columns.username })
+  }
+
+});
+
+const Student = defineTable({
+
+  columns:{
+    id_student: column.number({ primaryKey: true }),
+    username: column.text({ references: () => passw.columns.username })
+  }
+
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {passw, Profesor, Student},
 });
