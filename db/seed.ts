76eb938dce8304927ec36fcd5716bb0f5df34ff3
inwaytosenior <papatu10u18@gmail.com>
2024-05-15 22:18:29 -0600

    Configure el login y ya aparece el contenedor para ingresar el username y la contrase;a

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,11 @@
-import { db, passw } from 'astro:db';
+import { db, passw, Profesor, Student } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(passw).values([
+		{username: "Pablo24", password: "miracolous27"},
+		{username: "Maricarmen099", password: "chilerellenoconsalsa_2"},
+		{username: "JorgePamez", password: ""},
+		{username: "EstudiantePromedio2", password: "averagepassword2"},
+	])
 }
